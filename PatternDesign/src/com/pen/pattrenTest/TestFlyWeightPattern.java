fd9a43ed2c1322830c233b6f015007c0765fe6e2
pengchenwei <p1587600993@Gmail.com>
2016-08-23 20:22:33 +0800

    注解

--- PatternDesign/src/com/pen/pattrenTest/TestFlyWeightPattern.java
+++ PatternDesign/src/com/pen/pattrenTest/TestFlyWeightPattern.java
@@ -12,10 +12,13 @@ public class TestFlyWeightPattern {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 20; i++) {
+			//得到某个颜色的circle图形
 			CircleImpl circle = (CircleImpl) ShapeFactory.getShape(colorArr[(int) (Math.random() * colorArr.length)]);
+			//重新设置图形的参数
 			circle.setX((int) (Math.random() * 100));
 			circle.setY((int) (Math.random() * 100));
 			circle.setRadius(100);
+			//将图形draw出来
 			circle.draw();
 		}
 	}
